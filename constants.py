6a8ee025f2fa6104daab81493ae99ba20199b12f
Ali Oonwala <alioonwala@Alis-MacBook-Air-2.local>
2023-07-13 22:30:07 -0400

    Categories and prompt update

--- constants.py
+++ constants.py
@@ -1,17 +1,44 @@
 import config
 from polygon import ReferenceClient, StocksClient
 import requests
+import logging
 
 POLYGON_REFERENCE_CLIENT = ReferenceClient(config.polygon_key)
 POLYGON_STOCKS_CLIENT = StocksClient(config.polygon_key)
 
-# STOCK_LIST = [{'ticker': 'FNKO', 'name': 'FUNKO, INC.', 'market': 'stocks', 'locale': 'us', 'primary_exchange': 'XNYS', 'type': 'CS', 'active': True, 'currency_name': 'usd', 'cik': '0001090872', 'composite_figi': 'BBG000C2V3D6', 'share_class_figi': 'BBG001SCTQY4', 'last_updated_utc': '2023-06-27T00:00:00Z'}, {'ticker': 'A', 'name': 'Agilent Technologies Inc.', 'market': 'stocks', 'locale': 'us', 'primary_exchange': 'XNYS', 'type': 'CS', 'active': True, 'currency_name': 'usd', 'cik': '0001090872', 'composite_figi': 'BBG000C2V3D6', 'share_class_figi': 'BBG001SCTQY4', 'last_updated_utc': '2023-06-27T00:00:00Z'}, {'ticker': 'AA', 'name': 'Alcoa Corporation', 'market': 'stocks', 'locale': 'us', 'primary_exchange': 'XNYS', 'type': 'CS', 'active': True, 'currency_name': 'usd', 'cik': '0001675149', 'composite_figi': 'BBG00B3T3HD3', 'share_class_figi': 'BBG00B3T3HF1', 'last_updated_utc': '2023-06-27T00:00:00Z'}, {'ticker': 'AAA', 'name': 'AXS First Priority CLO Bond ETF', 'market': 'stocks', 'locale': 'us', 'primary_exchange': 'ARCX', 'type': 'ETF', 'active': True, 'currency_name': 'usd', 'cik': '0001776878', 'composite_figi': 'BBG01B0JRCS6', 'share_class_figi': 'BBG01B0JRCT5', 'last_updated_utc': '2023-06-27T00:00:00Z'}, {'ticker': 'AAAU', 'name': 'Goldman Sachs Physical Gold ETF Shares', 'market': 'stocks', 'locale': 'us', 'primary_exchange': 'BATS', 'type': 'ETF', 'active': True, 'currency_name': 'usd', 'cik': '0001708646', 'composite_figi': 'BBG00LPXX872', 'share_class_figi': 'BBG00LPXX8Z1', 'last_updated_utc': '2023-06-27T00:00:00Z'}]
+# ---Important Constants---
+ALERT_CATEGORIES = [
+    'FDA Drug Approval', 'FDA Drug Rejection', 'Clinical Trial Result', 'Merger/Acquisition'
+]
 
+# At least one of these keywords must be in the article headline for us to even process it. Case-insensitive
+HEADLINE_KEYWORDS = [
+    'fda authorization', 'approves', 'approval',    # FDA Drug Approval
+    'complete response letter',    # FDA Drug Rejection
+    'phase', 'clinical trial', 'trial results', 'study'    # Clinical Trial
+    'acquisition', 'acquire', 'merge', 'combine', 'combination', 'sell', 'join',    # Merger/Acquisition
+    # 'stock split',    # Stock Split
 
-# Exchanges we need to cover: XNAS XNYS XASE (NYSE and NASDAQ)
+]
 
+# ---Logging---
+# log.log will contain everything logged (above the logging level) in the most recent run of the program
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode='w',
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+# Info logging to info.log
+INFO_LOGGER = logging.getLogger('INFO_LOGGER')
+
+handler = logging.FileHandler('info.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+
+INFO_LOGGER.addHandler(handler)
+
+# ---Polygon Stock List---
 STOCK_LIST = []
 
+# Exchanges we need to cover: XNAS XNYS XASE (NYSE and NASDAQ)
 for exch in ('XNAS', 'XNYS', 'XASE'):
     poly_tickers = POLYGON_REFERENCE_CLIENT.get_tickers(market='stocks', exchange=exch, symbol_type='CS', limit=1000)
     next_url = poly_tickers.get('next_url', '')
